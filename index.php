80258617f8d7466d18fa8aa4121c20d0cbc007f8
maul-lq <rizlradfz@gmail.com>
2024-01-09 12:09:52 +0700

    halo dunia

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Card Name</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="X-UA-Compatible" content="IE=7" />
+    <meta name="description" content="" />
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body class="bg-slate-800">
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main>
+        <div class="judul w-full h-12 bg-gray-300 text-black text-2xl font-bold flex justify-center">
+            <h1>Tampilan Kartu Pengenal Siswa</h1>
+        </div>
+        <div class="list-card mt-2 mx-6 flex flex-row items-stretch justify-center gap-4">
+            <div class="card-name bg-slate-100 rounded-xl p-0 dark:bg-slate-800 flex flex-row">
+                <img class="w-48 h-auto rounded-xl object-cover mx-auto" src="./images/default-profile-picture.jpg" alt="Default Picture">
+                <div class="p-8 text-left space-y-4">
+                    <table class="">
+                        <tr>
+                            <td class="font-bold text-base">Nomor </td>
+                            <td class="font-normal text-base">: 1</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold text-base">NISN </td>
+                            <td class="font-normal text-base">: 2374628729</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold text-base">Tingkatan </td>
+                            <td class="font-normal text-base">: 12</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold text-base">Konsentrasi<br>Keahlian</td>
+                            <td class="font-normal text-base">: RPL</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold text-base">Wali Kelas </td>
+                            <td class="font-normal text-base">: Retno</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold text-base">Alamat Siswa </td>
+                            <td class="font-normal text-base">: Jl Bukit Duri Selatan</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold text-base">No. Telp Siswa </td>
+                            <td class="font-normal text-base">: +62 81585893549</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold text-base">Email </td>
+                            <td class="font-normal text-base">: 7e428c88@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold text-base">Nama Orangtua </td>
+                            <td class="font-normal text-base">: Waginah</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold text-base">No. Telp Orangtua </td>
+                            <td class="font-normal text-base">: +62 81932101784</td>
+                        </tr>
+                        <tr>
+                            <td class="font-bold text-base">Pekerjaan Orangtua </td>
+                            <td class="font-normal text-base">: Tidak ada</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+
+</body>
+
+</html>
